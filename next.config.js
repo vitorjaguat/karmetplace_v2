89332a34c245026a0260eae687eda545df5a368b
Vitor Butkus <vitorbutkus@gmail.com>
2025-11-13 13:36:24 -0300

    next.config redirects for older version routes

--- next.config.js
+++ next.config.js
@@ -12,9 +12,33 @@ const config = {
   async redirects() {
     return [
       {
-        source:
-          '/ethereum/asset/0x39f7e5bdfb46bf321b8df7803070d27d79361400:(:tokenId\\d+)',
-        destination: '/1/0x39f7e5bdfb46bf321b8df7803070d27d79361400/:tokenId',
+        source: '/ethereum/asset/0x39f7e5bdfb46bf321b8df7803070d27d79361400:1',
+        destination: '/1/0x39f7e5bdfb46bf321b8df7803070d27d79361400/1',
+        permanent: true,
+      },
+      {
+        source: '/ethereum/asset/0x39f7e5bdfb46bf321b8df7803070d27d79361400:2',
+        destination: '/1/0x39f7e5bdfb46bf321b8df7803070d27d79361400/2',
+        permanent: true,
+      },
+      {
+        source: '/ethereum/asset/0x39f7e5bdfb46bf321b8df7803070d27d79361400:3',
+        destination: '/1/0x39f7e5bdfb46bf321b8df7803070d27d79361400/3',
+        permanent: true,
+      },
+      {
+        source: '/ethereum/asset/0x39f7e5bdfb46bf321b8df7803070d27d79361400:4',
+        destination: '/1/0x39f7e5bdfb46bf321b8df7803070d27d79361400/4',
+        permanent: true,
+      },
+      {
+        source: '/ethereum/asset/0x39f7e5bdfb46bf321b8df7803070d27d79361400:5',
+        destination: '/1/0x39f7e5bdfb46bf321b8df7803070d27d79361400/5',
+        permanent: true,
+      },
+      {
+        source: '/ethereum/asset/0x39f7e5bdfb46bf321b8df7803070d27d79361400:6',
+        destination: '/1/0x39f7e5bdfb46bf321b8df7803070d27d79361400/6',
         permanent: true,
       },
     ];
