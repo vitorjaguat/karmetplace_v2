300de872d37745732d883109b2cc6d5c75e2ecdf
Vitor Butkus <vitorbutkus@gmail.com>
2025-11-13 13:30:27 -0300

    add redirects in next.config to handle v1 route structure

--- next.config.js
+++ next.config.js
@@ -9,6 +9,16 @@ const config = {
     '@0xsequence/marketplace-sdk',
     '@0xsequence/design-system',
   ],
+  async redirects() {
+    return [
+      {
+        source:
+          '/ethereum/asset/0x39f7e5bdfb46bf321b8df7803070d27d79361400:(:tokenId\\d+)',
+        destination: '/1/0x39f7e5bdfb46bf321b8df7803070d27d79361400/:tokenId',
+        permanent: true,
+      },
+    ];
+  },
 };
 
 export default withNextVideo(config);
